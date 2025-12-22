f2bd177f79c85b49b6c6ed9ad643236b8e127d08
Harsh-1302-ust <303368@ust.com>
2025-12-22 16:57:48 +0530

    updated TP_manager_Application_popup_coverletter

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TP_manager_Coverletter_popup from './components/TP_manager_Coverletter_popup'
+import TP_manager_Application_popup_coverletter from './components/TP_manager_Application_popup_coverletter'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <TP_manager_Coverletter_popup/>
+      <TP_manager_Application_popup_coverletter/>
     </>
   )
 }
