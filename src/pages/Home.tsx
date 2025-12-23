d59e0cf111b9e5826dac5693295382f8bb921b7c
ShyamSunderReddyB <303386@ust.com>
2025-12-23 17:23:52 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,21 @@
 import ApplicationStatus from '../components/Wfm/Home/ApplicationStatus'
+import { ActiveRoleContext } from './landing_page';
+import React from 'react';
 
 const Home = () => {
+  const { activeRole } = React.useContext(ActiveRoleContext);
   return (
-    <div className="min-h-screen bg-gray-50">
-    
-      <div className="max-w-7xl mx-auto p-6">
-        <div className="max-w-sm">
-          <ApplicationStatus />
+    <>
+    {activeRole === "WFM" && (
+        <div className="min-h-screen bg-gray-50">
+          <div className="max-w-7xl mx-auto p-6">
+            <div className="max-w-sm">
+              <ApplicationStatus />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 export default Home;
