ddcc24905b2a55248e9a38f797ce9dc88ee2ad40
Rohit2034 <Rohit.Kumar5@ust.com>
2025-12-23 13:42:09 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Application from './pages/Application';
 import './pages/landing_page'
-import LoginPage from './pages/landing_page';
+// import LoginPage from './pages/landing_page';
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-    <LoginPage />
+    {/* <LoginPage /> */}
+    <Application/>
+
     </>
     // <>
     //   <div>
