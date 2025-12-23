62cfc8e0e1e70fdd45aa2796a41c400eb448c88c
ShyamSunderReddyB <303386@ust.com>
2025-12-23 17:14:42 +0530

    integrated nav bar nad home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,15 @@
 import ApplicationStatus from '../components/Wfm/Home/ApplicationStatus'
-import { ActiveRoleContext } from './landing_page';
-import React from 'react';
 
 const Home = () => {
-  const { activeRole } = React.useContext(ActiveRoleContext);
   return (
-    <>
-    {activeRole === "WFM" && (
-        <div className="min-h-screen bg-gray-50">
-          <div className="max-w-7xl mx-auto p-6">
-            <div className="max-w-sm">
-              <ApplicationStatus />
-            </div>
-          </div>
+    <div className="min-h-screen bg-gray-50">
+    
+      <div className="max-w-7xl mx-auto p-6">
+        <div className="max-w-sm">
+          <ApplicationStatus />
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 };
 export default Home;
