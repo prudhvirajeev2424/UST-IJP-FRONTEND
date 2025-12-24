c51c812be9c95f0ae1ea6ba2350e1e4f314ed857
NiranjanReddy-Aare <303467@ust.com>
2025-12-24 09:18:46 +0530

    Assiging_and_tracking_page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css'
-import { ShortlistProvider } from './components/tp_manager/application/context/ShortlistContext';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/landing_page';
-import Home from './pages/home';
-import { ActiveRoleContext } from './context/ActiveRoleContext';
-import { useState } from 'react';
+import "./App.css";
+import { ShortlistProvider } from "./components/TP_Manager/application/context/ShortlistContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/landing_page";
+import Home from "./pages/home";
+import AssigningAndTracking from "./pages/AssigningandTracking";
+import { ActiveRoleContext } from "./context/ActiveRoleContext";
+import { useState } from "react";
 
 function App() {
   const [activeRole, setActiveRole] = useState<string | null>(null);
@@ -16,11 +17,12 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/assigning" element={<AssigningAndTracking />} />
           </Routes>
         </Router>
       </ActiveRoleContext.Provider>
     </ShortlistProvider>
-  )
+  );
 }
 
 export default App;
